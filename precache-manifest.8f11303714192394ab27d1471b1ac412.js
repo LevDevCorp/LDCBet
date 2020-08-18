4804edac5aac6a39a84febb71c1f5c0964cb49ea
LevDevCorp <L.nats51@yahoo.com>
2020-08-19 01:58:59 +0400

    Updates

---
+++ precache-manifest.8f11303714192394ab27d1471b1ac412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24b21472eb1d299149d235232c9e20e",
+    "url": "/leaderbet/index.html"
+  },
+  {
+    "revision": "c8cfa02faab0eb924ccb",
+    "url": "/leaderbet/static/css/main.a0af0c25.chunk.css"
+  },
+  {
+    "revision": "f4ca0a726c92b3248127",
+    "url": "/leaderbet/static/js/2.68cef739.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/leaderbet/static/js/2.68cef739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cfa02faab0eb924ccb",
+    "url": "/leaderbet/static/js/main.40dcdb59.chunk.js"
+  },
+  {
+    "revision": "4875f35ef51154ebf239",
+    "url": "/leaderbet/static/js/runtime-main.d0382feb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/LDCBet/precache-manifest.57074689ff312f855fc4c6b461e8d0ca.js"
+  "/leaderbet/precache-manifest.8f11303714192394ab27d1471b1ac412.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LDCBet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/leaderbet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
